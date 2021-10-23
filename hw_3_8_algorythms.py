8c15ebfec1faf8db9ab07adefa6998701827ccca
Vlad <zubun7@gmail.com>
2021-10-23 20:06:31 +0300

    6

--- hw_3_8_algorythms.py
+++ hw_3_8_algorythms.py
@@ -1,13 +1,35 @@
 #8. Матрица 5x4 заполняется вводом с клавиатуры кроме последних элементов строк.
 # Программа должна вычислять сумму введенных элементов каждой строки и записывать ее в последнюю ячейку строки.
 # В конце следует вывести полученную матрицу.
-
+import numpy as np
 def random_list():
     import random
     par_1 = int(input(f'Введите число строк матрицы: '))
     par_2 = int(input(f'Введите столбцов матрицы: '))
     #par_3 = int(input(f'Введите кол-во чисел в списке: '))
-    list = []
-    for i in range(par_3):
-        list.append(random.randint(par_1, par_2))
-    return list
+    list_matrix = []
+    for i in range(par_1):
+        line = []
+        list_matrix.append(line)
+        for i in range(par_2):
+            line.append(int(random.randint(0, 100)))
+
+    return list_matrix
+
+
+list_1 = random_list()
+
+new_matrix = []
+for i in range(len(list_1)):
+    summ = 0
+    matrix = []
+    new_matrix.append(matrix)
+    for j in range(len(list_1[i])):
+        summ += list_1[i][j]
+        matrix.append(list_1[i][j])
+    matrix.append(summ)
+
+#print(new_matrix)
+for i in range(len(new_matrix)):
+    print(f'{new_matrix[i]} \n')
+
--- hw_3_9_algorythms.py
+++ hw_3_9_algorythms.py
@@ -1,3 +1,14 @@
 #9. Найти максимальный элемент среди минимальных элементов столбцов матрицы.
 
-pip3 install numpy
+import numpy as np
+# def random_list():
+#     import random
+#     par_1 = int(input(f'Введите число строк матрицы: '))
+#     par_2 = int(input(f'Введите столбцов матрицы: '))
+#     #par_3 = int(input(f'Введите кол-во чисел в списке: '))
+#     list = []
+#     for i in range(par_3):
+#         list.append(random.randint(par_1, par_2))
+#     return list
+
+np.empty(10)
